532b6481affae66ad82ccbb903ac5e321c8d6d19
R10K5 <r10k5.job@gmail.com>
2023-11-25 13:34:53 +0300

    Добавлен CORS, получение карточке с прикреплением к ним типов и жанров, изменён способ получения данных с кинопоиска и записи их в бд

--- cards/views.py
+++ cards/views.py
@@ -3,7 +3,7 @@ import asyncio
 from django.core import serializers
 from django.http import HttpResponse, JsonResponse
 from kinopoisk_dev import KinopoiskDev, PossValField, MovieParams, MovieField
-from .models import Card, Type
+from .models import Card, Type, Genre
 from .tokens import TOKENS
 from asgiref.sync import sync_to_async
 
@@ -12,22 +12,19 @@ kp = KinopoiskDev(token=TOKENS['kinopoisk'])
 
 async def get_movies_async(type):
     params = [
-        MovieParams(keys=MovieField.PAGE, value=3),
-        MovieParams(keys=MovieField.LIMIT, value=1000),
+        MovieParams(keys=MovieField.PAGE, value=1),
+        MovieParams(keys=MovieField.LIMIT, value=250),
         MovieParams(keys=MovieField.TYPE, value=type),
     ]
 
     item = await kp.afind_many_movie(params=params)
     return item
 
-async def index(request):
-    print('1')
+async def write_kp(request):
     type = await sync_to_async(Type.objects.get, thread_sensitive=True)(pk=1)
-    print('1.1')
 
     movies = await get_movies_async(type.name)
     for movie in movies.docs:
-        print('3')
         cards = await sync_to_async(Card.objects.filter, thread_sensitive=True)(kp_id=movie.id)
         len_cards = await sync_to_async(len, thread_sensitive=True)(cards)
         if len_cards == 0:
@@ -37,11 +34,42 @@ async def index(request):
                         rate=movie.rating.kp,
                         year=movie.year,
                         kp_id=movie.id,
+                        duration_all=movie.movieLength or 0,
                         )
             await card.asave()
-            print('4')
             await sync_to_async(card.types.add, thread_sensitive=True)(type)
-            print('5')
-    print('gfdg12301250-09-')
 
-    return HttpResponse("OK")
+            genres = Genre.objects.all()
+            filtered_genres = []
+
+            for genre in movie.genres:
+                filtered = await sync_to_async(filter)(lambda g: g.name == genre.name, genres)
+                [g] = await sync_to_async(list)(filtered)
+                await sync_to_async(filtered_genres.append)(g)
+            await sync_to_async(card.genres.add, thread_sensitive=True)(*filtered_genres)
+
+    return HttpResponse('OK')
+
+def index(request):
+    result = []
+    cards = Card.objects.filter(types__in=[1])
+
+    for card in cards:
+        genres = list(map(lambda genre: genre.name, card.genres.all()))
+        types = list(map(lambda type: type.name, card.types.all()))
+
+        result.append({
+            'id': card.id,
+            'name': card.name,
+            'filename': card.filename,
+            'description': card.description,
+            'rate': card.rate,
+            'genres': genres,
+            'types': types,
+            'year': card.year,
+            'duration_all': card.duration_all,
+            'duration_series': card.duration_series,
+            'count_series': card.count_series,
+        })
+
+    return JsonResponse(result, safe=False)
--- pym/settings.py
+++ pym/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -103,6 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
 ]
 
 
